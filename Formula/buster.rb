0b348d35af563e656d8fdf219a2435f10312d9bc
Buster CLI Release Workflow <actions@github.com>
2025-05-31 15:50:50 +0000

    Update Formula/buster.rb for version v0.2.0

--- Formula/buster.rb
+++ Formula/buster.rb
@@ -4,7 +4,7 @@
 class Buster < Formula
   desc "Command-line interface for using buster Buster"
   homepage "https://github.com/buster-so/buster"
-  version "0.1.36"
+  version "0.2.0"
   license "MIT"
 
   livecheck do
@@ -15,18 +15,18 @@ class Buster < Formula
 
   on_macos do
     on_arm do
-      url "https://github.com/buster-so/buster/releases/download/v0.1.36/buster-cli-darwin-arm64.tar.gz"
-      sha256 "5b92db330607193da9fff72696511f23b7499dd203320fa953fe8dc518224dfa"
+      url "https://github.com/buster-so/buster/releases/download/v0.2.0/buster-cli-darwin-arm64.tar.gz"
+      sha256 "28f27f417b7e111abf6dfd4458c848910bf671e339e3be679d32752da35f60af"
     end
     on_intel do
-      url "https://github.com/buster-so/buster/releases/download/v0.1.36/buster-cli-darwin-x86_64.tar.gz"
-      sha256 "8a340c010276e78946d25b8f1b6cf48de1bf0aeb2616e3bde43f61d5da8c2320"
+      url "https://github.com/buster-so/buster/releases/download/v0.2.0/buster-cli-darwin-x86_64.tar.gz"
+      sha256 "a60a33f2dcfa770c013fbd29eea79b6c8d0a115f4396cdfb40549a25b5de4363"
     end
   end
 
   on_linux do
-    url "https://github.com/buster-so/buster/releases/download/v0.1.36/buster-cli-linux-x86_64.tar.gz"
-    sha256 "d002c4927709c349b8b0a40684495c9a30f5255497eccb8e13935ae3d4a9e85e"
+    url "https://github.com/buster-so/buster/releases/download/v0.2.0/buster-cli-linux-x86_64.tar.gz"
+    sha256 "45ae83464945674aae58905bd762485320ad185dfb9a30de575acda7a5d9b889"
   end
 
   def install
