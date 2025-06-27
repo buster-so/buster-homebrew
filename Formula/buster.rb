58299d202f7cea4e220a4d9e3276d1ba7432b609
Buster CLI Release Workflow <actions@github.com>
2025-06-27 22:56:31 +0000

    Update Formula/buster.rb for version v0.2.3

--- Formula/buster.rb
+++ Formula/buster.rb
@@ -16,17 +16,17 @@ class Buster < Formula
   on_macos do
     on_arm do
       url "https://github.com/buster-so/buster/releases/download/v0.2.3/buster-cli-darwin-arm64.tar.gz"
-      sha256 "3048027596e2f4f6daaa7641e0839ba45c889e133fb381189ee41bb7e60ba28d"
+      sha256 "44765231757d0b0f551f54d4a4b064ce60927c09a2717d8c6f3be0779c927882"
     end
     on_intel do
       url "https://github.com/buster-so/buster/releases/download/v0.2.3/buster-cli-darwin-x86_64.tar.gz"
-      sha256 "a8bcf45ede11ac48dfad5508ab123d0f57e546c0d4dc5edb0da609e775ca0ebe"
+      sha256 "b5c69d82faedd8189e606f86398404ce374cb2aaf0e31a4b3645358db21204ff"
     end
   end
 
   on_linux do
     url "https://github.com/buster-so/buster/releases/download/v0.2.3/buster-cli-linux-x86_64.tar.gz"
-    sha256 "d059afbb9db003eed208667e5c959e1bfce2e28937906fa68501b8131909cbcd"
+    sha256 "1385783d75a1cb3b624f6975e8cf4d9f235d993e6577e210754e407a5086bec8"
   end
 
   def install
